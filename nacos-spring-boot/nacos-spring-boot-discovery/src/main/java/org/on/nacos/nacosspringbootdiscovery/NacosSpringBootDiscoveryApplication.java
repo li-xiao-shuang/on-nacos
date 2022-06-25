3c91794afe14399ed679de3a2ff1e18f8b53b540
lixiaoshuang <644968328@qq.com>
2022-06-25 23:23:15 +0800

    调整结构

---
+++ nacos-spring-boot/nacos-spring-boot-discovery/src/main/java/org/on/nacos/nacosspringbootdiscovery/NacosSpringBootDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package org.on.nacos.nacosspringbootdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosSpringBootDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosSpringBootDiscoveryApplication.class, args);
+	}
+
+}
---
+++ nacos-spring-boot/nacos-spring-boot-discovery/src/test/java/org/on/nacos/nacosspringbootdiscovery/NacosSpringBootDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.on.nacos.nacosspringbootdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosSpringBootDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
